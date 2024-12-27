eed53a341f35428d5e6c60019af8c00da78f3439
Beibit <asylbekulybeibit@gmail.com>
2024-12-27 18:52:32 +0500

    туцтуц

--- bereshek-api/src/users/users.controller.ts
+++ bereshek-api/src/users/users.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
+@ApiTags('users')
+@ApiBearerAuth()
 @Controller('users')
 @UseGuards(JwtAuthGuard)
 export class UsersController {
